ec88ef29d779ed69239cf463a9cd97a01b5a87f7
Abdullah Butt <abdullah.butt.dev.github@gmail.com>
2026-01-24 13:13:55 +0500

    refactor asyncHandler functions to standardize error handling and improve readability

--- server/src/utils/asyncHandlerTrycatch.js
+++ server/src/utils/asyncHandlerTrycatch.js
@@ -1,14 +1,9 @@
-
-
 const asyncHandler = (func) => async (req, res, next) => {
     try {
         await func(req, res, next);
     } catch (error) {
-        res.status(error.code || 500).json({
-            success: false,
-            message: error.message || "Internal Server Error"
-        })
+        next(error); // ✅ HERE
     }
-}
+};
 
 export default asyncHandler;
