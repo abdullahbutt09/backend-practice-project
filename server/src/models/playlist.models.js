cb82051c70bd81c6fb1f211d7305d229adb28e51
Abdullah Butt <abdullah.butt.dev.github@gmail.com>
2026-02-05 08:19:47 +0500

    add like and playlist models with schemas for likes and playlists

---
+++ server/src/models/playlist.models.js
@@ -0,0 +1,28 @@
+import mongoose, {Schema} from "mongoose";
+
+const playlistSchema = new Schema({
+
+    playlistName:{
+        type: String,
+        required: true
+    },
+
+    playlistDescription:{
+        type: String,
+    },
+
+    videos:[
+        {
+            type: Schema.Types.ObjectId,
+            ref: "Video"
+        },
+    ],
+
+    owner:{
+        type: Schema.Types.ObjectId,
+        ref: "User"
+    },
+    
+}, {timestamps: true});
+
+export const Playlist = mongoose.model("Playlist", playlistSchema);
